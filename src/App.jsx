7c6c914aa40a40353ed800fa580111a3c1cf0928
Crazeecoder01 <121546558+Crazeecoder01@users.noreply.github.com>
2023-12-22 23:20:17 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Himanshu Gupta",
+  title: "Web Developer and Programmer",
+  email: "Himanshu.Gupta@studentambassadors.com",
+  gitHub: "crazeecoder01",
+  instagram: "guptahimanshu177",
+  linkedIn: "HimanshuGupta",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "himanshugupta",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,10 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/avenue.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
-
+const imageAltText = "Beautiful picturesque morning scene. The sunlight is gazing through the spaces of the woods to create a mesmerizing morning set."
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
