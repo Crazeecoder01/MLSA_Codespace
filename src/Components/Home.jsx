8470c48999ed86c49764099e8fe8737a07e9cbd4
Crazeecoder01 <121546558+Crazeecoder01@users.noreply.github.com>
2023-12-22 23:27:56 +0530

    new changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/avenue.jpg";
+import image from "../images/mountains.jpg";
 
 const imageAltText = "Beautiful picturesque morning scene. The sunlight is gazing through the spaces of the woods to create a mesmerizing morning set."
 const Home = ({ name, title }) => {
